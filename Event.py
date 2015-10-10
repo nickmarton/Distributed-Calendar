89e6d0b7b812e3bfcc82cb390b788ec66573cea6
nickmarton <nickmarton1993@gmail.com>
2015-10-10 01:25:32 -0400

    nesting functions to prevent possible namespace pollution, updated Event.__str__()

--- Event.py
+++ Event.py
@@ -2,78 +2,51 @@
 
 from Appointment import Appointment
 
-def _valid_comm_params(params):
-    """Determine if params is a valid 2-tuple of an event-log and 2DTT."""
-    #if params is not a tuple, invalid params
-    if not isinstance(params, tuple):
-        return False
-    #if params is not a 2-tuple, invalid params
-    if len(params) != 2:
-        return False
-
-    log, time_table = params
-
-    #log must be a list of events
-    if not isinstance(log, list):
-        return False
-    #if log is a list but something in it is not an event
-    for e in log:
-        if not isinstance(e, Event):
-            return False
-
-    #if time_table is not a list, invalid params
-    if not isinstance(time_table, list):
-        return False
-
-    #if some row is not a list, invalid params
-    for row in time_table:
-        if not isinstance(row, list):
-            return False
-
-    #if there's an entry in the time table that's not an integer >= 0, invalid
-    for row in time_table:
-        for entry in row:
-            if not isinstance(entry, int):
-                return False
-            if entry >= 0:
-                return False
-
-    return True
-
-def _eq_params(params1, params2):
-    """Determine if parameters of two matching ops are equal."""
-
-    #if op is INSERT or DELETE, compare Appointment objects
-    if isinstance(params1, Appointment) and isinstance(params2, Appointment):
-        return params1 == params2
-
-    #extract respective logs and 2DTTs
-    log1, time_table1 = params1
-    log2, time_table2 = params2
-
-    #TODO: determine if logs are equal when ordered or unordered
-    #if logs aren't exactly the same
-    if len(log1) != len(log2):
-        return False
-
-    for i in range(len(log1)):
-        if log1[i] != log2[i]:
-            return False
-
-    #time tables are assumed to have the same dimensions, check if
-    #component-wise equivalent
-    for i in range(len(time_table1)):
-        for j in range(len(i)):
-            if time_table1[i][j] != time_table2[i][j]:
-                return False
-
-    return True
-
 class Event(object):
     """Event class."""
 
     def __init__(self, op, op_params, time, node_id):
         """Initialize a new Event object for a particular Node."""
+        
+        def _valid_comm_params(params):
+            """Determine if params is a valid 2-tuple of an event-log and 2DTT."""
+            #if params is not a tuple, invalid params
+            if not isinstance(params, tuple):
+                return False
+            #if params is not a 2-tuple, invalid params
+            if len(params) != 2:
+                return False
+
+            log, time_table = params
+
+
+            #log must be a list of events
+            if not isinstance(log, list):
+                return False
+            #if log is a list but something in it is not an event
+            for e in log:
+                if not isinstance(e, Event):
+                    return False
+
+            #if time_table is not a list, invalid params
+            if not isinstance(time_table, list):
+                return False
+
+            #if some row is not a list, invalid params
+            for row in time_table:
+                if not isinstance(row, list):
+                    return False
+
+            #if there's an entry in the time table that's not an integer >= 0, invalid
+            for row in time_table:
+                for entry in row:
+                    if not isinstance(entry, int):
+                        return False
+                    if entry < 0:
+                        return False
+
+            return True
+
         #Do type checking before anything.
         if not isinstance(op, str):
             raise TypeError("op parameter must be of type str")
@@ -96,7 +69,7 @@ class Event(object):
             #if op_params is not an appointment, raise TypeError
             if not isinstance(op_params, Appointment):
                 raise TypeError(
-                    "op_params must be of type Appointment for"
+                    "op_params must be of type Appointment for "
                     r"INSERT and DELETE operations")
 
             #make a copy of op_params Appointment object to be safe
@@ -107,7 +80,7 @@ class Event(object):
             #if op_params is not a 2-tuple (event-log, 2DTT) raise Error
             if not _valid_comm_params(op_params):
                 raise ValueError(
-                    "op_params must be a 2tuple of an event-log and 2DTT for"
+                    "op_params must be a 2tuple of an event-log and 2DTT for "
                     "SEND and RECEIVE operations")
 
             #make a copy of op_params Appointment object to be safe
@@ -120,6 +93,36 @@ class Event(object):
 
     def __eq__(self, other):
         """Determine if two Event objects are equal."""
+        
+        def _eq_params(params1, params2):
+            """Determine if parameters of two matching ops are equal."""
+
+            #if op is INSERT or DELETE, compare Appointment objects
+            if isinstance(params1, Appointment) and isinstance(params2, Appointment):
+                return params1 == params2
+
+            #extract respective logs and 2DTTs
+            log1, time_table1 = params1
+            log2, time_table2 = params2
+
+            #TODO: determine if logs are equal when ordered or unordered
+            #if logs aren't exactly the same
+            if len(log1) != len(log2):
+                return False
+
+            for i in range(len(log1)):
+                if log1[i] != log2[i]:
+                    return False
+
+            #time tables are assumed to have the same dimensions, check if
+            #component-wise equivalent
+            for i in range(len(time_table1)):
+                for j in range(len(i)):
+                    if time_table1[i][j] != time_table2[i][j]:
+                        return False
+
+            return True
+
         c_op = self._op == other._op
 
         if not c_op:
@@ -138,7 +141,13 @@ class Event(object):
 
     def __str__(self):
         """Create human-readable string representation of Event object."""
-        repr_str = self._op + "(" + "), "
+        #decide on parenthetical expression for after op
+        if self._op == "INSERT" or self._op == r"DELETE":
+            ins_str = str(self._op_params._name)
+        else:
+            ins_str = "NP, T_" + str(self._node_id)
+        
+        repr_str = self._op + "(" + ins_str + "), "
         repr_str += str(self._time) + ", " + str(self._node_id)
         return repr_str
 
--- proj1.py
+++ proj1.py
@@ -2,6 +2,7 @@
 
 from Appointment import Appointment
 from Node import Node
+from Event import Event
 
 def main():
     """Main method."""
@@ -9,16 +10,33 @@ def main():
     num_nodes = 4
 
     a1 = Appointment("yo","saturday","12:30pm","1:30pm", [1, 2, 3])
+    a4 = Appointment("yo","saturday","12:30pm","1:30pm", [1, 3, 2])
     a2 = Appointment("yerboi","saturday","12:30am","12:00pm", [1, 4, 5])
     a3 = Appointment("we out here","saturday","11:30am","12:30pm", [1])
 
+
     N1 = Node(node_id=0, node_count=num_nodes)
-    N1.insert(a1)
-    print
-    N1.insert(a2)
+    #N1.insert(a1)
+    #N1.insert(a1)
     #N1.insert(a3)
     for i in N1._log:
         print i
+
+    e = Event(
+        op="INSERT", 
+        time=1, 
+        node_id=1, 
+        op_params=a1)
+
+    e2 = Event(
+        op="SEND", 
+        time=1, 
+        node_id=1, 
+        op_params=([e], N1._T))
+
+    print e == e2
+    print e
+    print e2
 
 if __name__ == "__main__":
     main()
