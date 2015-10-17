17d52e325358f6e99fa46a72b7766271d8f6c875
Ian OBoyle <oboyle.ian@gmail.com>
2015-10-17 17:05:24 -0400

    Added test client

---
+++ clientTest.py
@@ -0,0 +1,27 @@
+import socket
+
+HOST = ""
+PORT = 1024
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+sock.connect((HOST, PORT))
+
+while(1):
+    print("Client started")
+    BUFFER_SIZE = 1024
+    MESSAGE = raw_input("")
+
+    if ( MESSAGE == "quit" or MESSAGE == "terminate"):
+    	sock.send(MESSAGE)
+    	break
+
+    sock.send(MESSAGE)
+
+    data = sock.recv(BUFFER_SIZE)
+    data = data.decode('UTF-8')
+    print "Recieved data:" + data
+
+sock.close()
+    
